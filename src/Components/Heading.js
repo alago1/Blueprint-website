414cda40c2e3b73cb97fbd677bc119cecfb03961
Allan Lago <allanlago1@gmail.com>
2020-06-08 16:27:26 -0400

    Fixed heading name stroke-width

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -1,13 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { ReactComponent as HeaderName } from "../svgs/1. Header/Header_Name.svg";
 import HeaderNav from "./HeaderNav";
 import DrawSVG from "./Animation/DrawSVG";
 import "./Styles/Heading.css";
 
 function Heading() {
+  const [rendered, setRendered] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setRendered(true);
+    }, 4500);
+  });
+
   return (
     <Fragment>
-      <section className="name">
+      <section className={rendered ? "name lowstroke" : "name"}>
         <DrawSVG duration="3000" delay="1500">
           <HeaderName />
         </DrawSVG>
